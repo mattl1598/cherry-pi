4734245054e1e0f70539af4a7d8809ac30e06bc8
Matthew Larby <larbymo@cardiff.ac.uk>
2020-11-11 00:25:03 +0000

    testing another js function

--- webapp/static/js/sp_blog.js
+++ webapp/static/js/sp_blog.js
@@ -1,29 +1,23 @@
 function getPost(id) {
-    var xmlhttp = new XMLHttpRequest();
-    var url = 'http://larby.co.uk/sp-post?src=js&post=' + id;
+    if (id != 0) {
+        var xmlhttp = new XMLHttpRequest();
+        var url = 'http://larby.co.uk/sp-post?src=js&post=' + id;
 
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
-            display(myArr);
-        }
-    };
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var myArr = JSON.parse(this.responseText);
+                display(myArr);
+            }
+        };
 
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    } else {
+        display({"html_content": "List of Posts"})
+    }
 }
 
 function display(arr) {
     var out = arr["html_content"];
     document.getElementById("sp_blog_content").innerHTML = out;
-}
-
-function get_sp_post(id) {
-    var url = 'http://larby.co.uk/sp-post?src=js&post=' + id;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            var myArr = JSON.parse(data);
-            display(myArr);
-        });
 }
