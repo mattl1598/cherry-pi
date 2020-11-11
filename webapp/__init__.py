49e7f7c0c96ab4367533fab41dad6fa697651ba3
Matthew Larby <larbymo@cardiff.ac.uk>
2020-11-11 00:09:45 +0000

    updated cors policy

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,7 @@
 #!/var/www/cherry-pi-prod/venv
 
 import sys
+
 print(sys.prefix)
 
 from flask import Flask
@@ -38,16 +39,19 @@ app.static_folder = 'static'
 app.config['SECRET_KEY'] = env_vars["secret_key"]
 app.config['SQLALCHEMY_DATABASE_URI'] = env_vars["postgresql"]
 
-app.jinja_env.globals.update(test=test_script, date=datetime.datetime.now, key64=key_64, dump=json.dumps, int=int, isinstance=isinstance)
+app.jinja_env.globals.update(test=test_script, date=datetime.datetime.now, key64=key_64, dump=json.dumps, int=int,
+								isinstance=isinstance)
 
-cors = CORS(app, resources={r"/js/*": {"origins": "http://silchesterplayers.org"}, r"/listens/*": {"origins": "http://silchesterplayers.org"}})
+cors = CORS(app, resources={r"/js/*": {"origins": "http://silchesterplayers.org"},
+							r"/listens/*": {"origins": "http://silchesterplayers.org"},
+							r"/sp-post*": {"origins": "http://silchesterplayers.org"}
+							})
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 nav = Navigation()
 nav.init_app(app)
-
 
 from webapp import routes
 
